a1ee035da06e094f43ff7b02f497a361c14f76bd
jmaris <jmaris@kydara.com>
2016-03-28 18:31:00 +0200

    fixed login notification scropt

--- login-notification-telegram.bash
+++ login-notification-telegram.bash
@@ -24,7 +24,7 @@ DATE=$(date)
 ### TELEGRAM SETTINGS ###
 TELEGRAM_TOKEN=194823705:AAF2q7oFk0D1-DNn9GOh6zDkcBLFN7Q18C4
 TELEGRAM_USERID=32862227
-TELEGRAM_MESSAGE=$HOSTNAME "Login notification : User <b>$PAM_USER</b> opened a session from <b>$PAM_RUSER@$PAM_RHOST</b> through <b>$PAM_SERVICE</b> at <i>$DATE</i>."
+TELEGRAM_MESSAGE="$HOSTNAME Login notification : User <b>$PAM_USER</b> opened a session from <b>$PAM_RUSER@$PAM_RHOST</b> through <b>$PAM_SERVICE</b> at <i>$DATE</i>."
 ### ENDOF TELEGRAM SETTINGS ###
 
 curl --data "chat_id=$TELEGRAM_USERID&parse_mode=html" --data-urlencode "text=$TELEGRAM_MESSAGE"  "https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendMessage"  > /dev/null 2>&1 &
